f8eff4f1144cc1a193582b2f8b9032a253c9a8e7
ZaitsWit <vasiljev.hardman@yandex.ru>
2022-09-13 12:06:49 +0300

    убраны все if'ы, сокращен подсчет матрицы

--- summ_of_matrix/summ_of_matrix.py
+++ summ_of_matrix/summ_of_matrix.py
@@ -9,22 +9,6 @@ final_matrix = []
 for i in range(len(matrix)):
     final_matrix.append([])
     for j in range(len(matrix[i])):
-        final_matrix[i].append(0)
-        if i > 0:
-            final_matrix[i][j] += int(matrix[i - 1][j])
-        else:
-            final_matrix[i][j] += int(matrix[- 1][j])
-        if i == len(matrix) - 1:
-            final_matrix[i][j] += int(matrix[0][j])
-        else:
-            final_matrix[i][j] += int(matrix[i + 1][j])
-        if j > 0:
-            final_matrix[i][j] += int(matrix[i][j - 1])
-        else:
-            final_matrix[i][j] += int(matrix[i][- 1])
-        if j == len(matrix[i]) - 1:
-            final_matrix[i][j] += int(matrix[i][0])
-        else:
-            final_matrix[i][j] += int(matrix[i][j + 1])
+        final_matrix[i].append(int(matrix[i - 1][j]) + int(matrix[i + 1 - len(matrix)][j]) + int(matrix[i][j - 1]) + int(matrix[i][j + 1 - len(matrix[i])]))
         print(final_matrix[i][j],  ' ', end='')
     print()
